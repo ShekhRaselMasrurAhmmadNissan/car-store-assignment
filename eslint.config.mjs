4f2543f1544b01091ac0f963960aa912b0c55b15
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2024-11-24 23:43:14 +0600

    Made some setting changes and configure eslint and prettier.

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+// eslint.config.mjs
+import pluginJs from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts}'] },
+	{ files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		ignores: ['node_modules', 'dist'],
+		rules: {
+			'no-unused-vars': 'error',
+			'no-unused-expressions': 'error',
+			'prefer-const': 'error',
+			'no-console': 'warn',
+			'no-undef': 'error',
+		},
+	},
+	eslintPluginPrettierRecommended,
+];
