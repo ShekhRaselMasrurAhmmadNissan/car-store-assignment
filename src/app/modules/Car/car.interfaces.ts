36dddf7602a6c5a52bae6c6575cd8a94b56fdd3b
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2024-11-25 10:49:13 +0600

    Car Interface and Car Model Created Complete.

---
+++ src/app/modules/Car/car.interfaces.ts
@@ -0,0 +1,26 @@
+import { Model } from 'mongoose';
+
+// Enum for Category
+export enum Category {
+	Sedan = 'Sedan',
+	SUV = 'SUV',
+	Truck = 'Truck',
+	Coupe = 'Coupe',
+	Convertible = 'Convertible',
+}
+
+// Interface for Car
+export interface ICar {
+	brand: string;
+	model: string;
+	year: number;
+	price: number;
+	category: string;
+	description: string;
+	quantity: number;
+	inStock: boolean;
+}
+
+export interface ICarModel extends Model<ICar> {
+	isCarExists(id: string): Promise<ICar | null>;
+}
---
+++ src/app/modules/Car/car.model.ts
@@ -0,0 +1,55 @@
+import { model, Schema } from 'mongoose';
+import { Category, ICar, ICarModel } from './car.interfaces';
+
+const carSchema = new Schema<ICar, ICarModel>(
+	{
+		brand: {
+			type: String,
+			required: true,
+		},
+		model: {
+			type: String,
+			required: true,
+		},
+		year: {
+			type: Number,
+			required: true,
+		},
+		price: {
+			type: Number,
+			required: true,
+		},
+		category: {
+			type: String,
+			required: true,
+			enum: {
+				values: Object.values(Category),
+			},
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		quantity: {
+			type: Number,
+			required: true,
+			min: 0,
+		},
+		inStock: {
+			type: Boolean,
+			default: true,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	},
+);
+
+// * Static Method
+carSchema.statics.isCarExists = async (id: string): Promise<ICar | null> => {
+	const car = await Car.findById(id);
+	return car;
+};
+
+export const Car = model<ICar, ICarModel>('Car', carSchema);
